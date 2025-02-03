146ccdbf0c39e1c83c45a427dfbedef5847012b3
EJ <harrygina09@naver.com>
2025-02-03 11:23:48 +0900

    유저추가

--- src/users/AddUser.jsx
+++ src/users/AddUser.jsx
@@ -1,60 +1,90 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import axios from "axios";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 function AddUser() {
+  const navigate = useNavigate(); //라우터 이동객체
+  const [user, setUser] = useState({
+    name: "",
+    username: "",
+    email: "",
+  });
+  const { name, username, email } = user;
+  const onInputChange = (e) => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    await axios.post("http://localhost:8080/users", user);
+    navigate("/"); //기본 페이지로 이동
+  };
   return (
     <div className="container">
       <div className="row">
         <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
           <h2 className="text-center m-4">가입 하기</h2>
-          <div className="mb-3">
-            <label htmlFor="name" className="form-label">
-              이름
-            </label>
-            <input
-              type="text"
-              id="name"
-              className="form-control"
-              placeholder="이름 입력"
-              name="name"
-            />
-            <label htmlFor="username" className="form-label">
-              유저네임
-            </label>
-            <input
-              type="text"
-              id="username"
-              className="form-control"
-              placeholder="유저네임 입력"
-              name="username"
-            />
-            <label htmlFor="email" className="form-label">
-              이메일
-            </label>
-            <input
-              type="email"
-              id="email"
-              className="form-control"
-              placeholder="이메일 입력"
-              name="email"
-            />
+          <form onSubmit={onSubmit}>
+            <div className="mb-3">
+              <label htmlFor="name" className="form-label">
+                이름
+              </label>
+              <input
+                required
+                onChange={onInputChange}
+                value={name}
+                type="text"
+                id="name"
+                className="form-control"
+                placeholder="이름 입력"
+                name="name"
+              />
+              <label htmlFor="username" className="form-label">
+                유저네임
+              </label>
+              <input
+                required
+                onChange={onInputChange}
+                value={username}
+                type="text"
+                id="username"
+                className="form-control"
+                placeholder="유저네임 입력"
+                name="username"
+              />
+              <label htmlFor="email" className="form-label">
+                이메일
+              </label>
+              <input
+                required
+                onChange={onInputChange}
+                value={email}
+                type="email"
+                id="email"
+                className="form-control"
+                placeholder="이메일 입력"
+                name="email"
+              />
 
-            <div className="mt-3 text-center">
-              <button
-                type="submit"
-                className="btn btn-outline-primary px-3 mx-2"
-              >
-                가입
-              </button>
-              <Link
-                to="/"
-                type="submit"
-                className="btn btn-outline-danger px-3 mx-2"
-              >
-                취소
-              </Link>
+              <div className="mt-3 text-center">
+                <button
+                  type="submit"
+                  className="btn btn-outline-primary px-3 mx-2"
+                >
+                  가입
+                </button>
+                <Link
+                  to="/"
+                  type="submit"
+                  className="btn btn-outline-danger px-3 mx-2"
+                >
+                  취소
+                </Link>
+              </div>
             </div>
-          </div>
+          </form>
         </div>
       </div>
     </div>
